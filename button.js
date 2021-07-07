8e6fbd54fa033a67f9a854e040acd956f90176cf
elvysd <elvysd@gmail.com>
2021-07-06 20:38:25 -0700

    Basic functionality added

--- button.js
+++ button.js
@@ -1,1 +1,24 @@
+function numberEntry(event) {
+    var x = event.key;
+    console.log(x);
+    if (x > 31 && (x < 48 || x > 57)) {
+        event.preventDefault();
+        console.log(x);
+    }
+}
+
+
+
+function buttonDown(num) {
+    let s = document.getElementById("s").value;
+    document.getElementById("s").innerHTML = s;
+    console.log(s);
+
+    document.getElementById("s").value =  num +=document.getElementById("s").value;
+
+
+}
+
+
+console.log(s);
 
